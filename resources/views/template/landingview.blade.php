435d1c70b10d00d4b862dd92922110fc6bbcd1c6
Adhi MF <66113867+adhiiimf@users.noreply.github.com>
2021-05-17 23:36:12 +0700

    Add first landing master

---
+++ resources/views/template/landingview.blade.php
@@ -0,0 +1,7 @@
+<section id="landingview">
+    <div class="container">
+        <div class="text-center">
+            <img class="img-fluid logomain-size" src="assets/landing-main-logo-red.png" alt="">
+        </div>
+    </div>
+</section>
