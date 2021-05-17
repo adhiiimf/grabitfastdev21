ef580527d6bc399f08bd73f3e6512540d10ef2b5
Adhi MF <66113867+adhiiimf@users.noreply.github.com>
2021-05-17 23:52:27 +0700

    Remove link of medspor css

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -10,7 +10,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="/styles/landing.css">
-    <link rel="stylesheet" href="/styles/medspor/medpartsponsor.css">
     <link rel="stylesheet" href="/styles/navbar.css">
     <link rel="stylesheet" href="/styles/sticky-oa.css">
     <link rel="preconnect" href="/https://fonts.gstatic.com">
