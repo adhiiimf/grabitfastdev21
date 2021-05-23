16ffcb0b809f511274371cb1e0151329e9477d06
Adhi MF <66113867+adhiiimf@users.noreply.github.com>
2021-05-23 22:15:06 +0700

    Add link for medpart and sponsor

--- resources/views/template/medspor.blade.php
+++ resources/views/template/medspor.blade.php
@@ -11,17 +11,17 @@
                     <div class="row">
                     <div class="col-lg text-center pm-medspor">
                         <div>
-                        <img class="spor-logo" src="assets/sponsors/Antam.png" alt="">
+                        <a href="https://www.antam.com/en" target="_blank"><img class="spor-logo" src="assets/sponsors/Antam.png" alt="PT Antam Tbk"></a>
                         </div>
                     </div>
                     <div class="col-lg text-center pm-medspor">
                         <div>
-                        <img class="spor-logo" src="assets/sponsors/telkomindonesia.png" alt="">
+                        <a href="https://www.telkom.co.id/sites" target="_blank"><img class="spor-logo" src="assets/sponsors/telkomindonesia.png" alt="Telkom Indonesia"></a> 
                         </div>
                     </div>
                     <div class="col-lg text-center pm-medspor">
                         <div>
-                        <img class="spor-logo" src="assets/sponsors/BNI.png" alt="">
+                        <a href="https://www.bni.co.id/id-id/" target="_blank"><img class="spor-logo" src="assets/sponsors/BNI.png" alt="BNI"></a>
                         </div>
                     </div>
                 </div>  
@@ -30,17 +30,17 @@
                 <div class="row">
                 <div class="col-lg text-center pm-medspor">
                     <div>
-                    <img class="spor-logo" src="assets/sponsors/Logo Teman Sedekah Indonesia.png" alt="">
+                    <a href="https://www.instagram.com/teman_sedekah_indonesia/" target="_blank"><img class="spor-logo" src="assets/sponsors/Logo Teman Sedekah Indonesia.png" alt="Teman Sedekah Indonesia"></a>
                     </div>
                 </div>
                 <div class="col-lg text-center pm-medspor">
                     <div>
-                    <img class="spor-logo" src="assets/sponsors/Logo-Wardah.jpeg" alt="">
+                    <a href="https://www.wardahbeauty.com/id" target="_blank"><img class="spor-logo" src="assets/sponsors/Logo-Wardah.jpeg" alt="Wardah Cosmetic"></a>
                     </div>
                 </div>
                 <div class="col-lg text-center pm-medspor">
                     <div>
-                    <img class="spor-logo" src="assets/sponsors/EF.png" alt="">
+                    <a href="https://www.ef.co.id/" target="_blank"><img class="spor-logo" src="assets/sponsors/EF.png" alt="EF"></a>
                     </div>
                 </div>
             </div> 
@@ -49,17 +49,17 @@
             <div class="row">
             <div class="col-lg text-center pm-medspor">
                 <div>
-                <img class="spor-logo" src="assets/sponsors/ruru snack.jpg" alt="">
+                <a href="https://www.instagram.com/rurusnack/" target="_blank"><img class="spor-logo" src="assets/sponsors/ruru snack.jpg" alt="Ruru Snack"></a>
                 </div>
             </div>
             <div class="col-lg text-center pm-medspor">
                 <div>
-                <img class="spor-logo" src="assets/sponsors/1000x1000/BOZOON 1000x1000.png" alt="">
-                </div>
+                <a href="https://booz-on.com/" target="_blank"><img class="spor-logo" src="assets/sponsors/1000x1000/BOZOON 1000x1000.png" alt="">
+                </div></a>
             </div>
             <div class="col-lg text-center pm-medspor">
                 <div>
-                <img class="spor-logo" src="assets/sponsors/Grow Vibes Music.png" alt="">
+                <a href="https://www.instagram.com/growvibesmusic/" target="_blank"><img class="spor-logo" src="assets/sponsors/Grow Vibes Music.png" alt="Grow Vibes Music"></a>
                 </div>
             </div>
         </div> 
@@ -73,22 +73,17 @@
                     <div class="row">
                     <div class="col-lg text-center pm-medspor">
                         <div>
-                        <img class="medspor-logo" src="assets/medpart/concertmusic.jpg" alt="">
+                        <a href="https://www.instagram.com/concertmusic_/" target="_blank"><img class="medspor-logo" src="assets/medpart/concertmusic.jpg" alt="ConcertMusic"></a>
                         </div>
                     </div>
                     <div class="col-lg text-center pm-medspor">
                         <div>
-                        <img class="medspor-logo" src="assets/medpart/eventbandung.jpg" alt="">
+                        <a href="https://www.instagram.com/eventbandungid/" target="_blank"><img class="medspor-logo" src="assets/medpart/eventbandung.jpg" alt="Event Bandung"></a>
                         </div>
                     </div>
                     <div class="col-lg text-center pm-medspor">
                         <div>
-                        <img class="medspor-logo" src="assets/medpart/shareevent.jpg" alt="">
-                        </div>
-                    </div>
-                    <div class="col-lg text-center pm-medspor">
-                        <div>
-                        <img class="medspor-logo" src="assets/medpart/ikomchannel.jpg" alt="">
+                        <a href="https://www.instagram.com/share_event/" target="_blank"><img class="medspor-logo" src="assets/medpart/shareevent.jpg" alt="shareevent"></a>
                         </div>
                     </div>
                 </div>  
@@ -97,27 +92,27 @@
                 <div class="row">
                 <div class="col-lg text-center pm-medspor">
                     <div>
-                    <img class="medspor-logo" src="assets/medpart/motauevent.jpg" alt="">
+                    <a href="https://www.instagram.com/motau_event/" target="_blank"><img class="medspor-logo" src="assets/medpart/motauevent.jpg" alt="motauevent"></a>
                     </div>
                 </div>
                 <div class="col-lg text-center pm-medspor">
                     <div>
-                    <img class="medspor-logo" src="assets/medpart/imagi.jpg" alt="">
+                    <a href="https://www.instagram.com/imagi_fik/" target="_blank"><img class="medspor-logo" src="assets/medpart/imagi.jpg" alt="imagi_fik"></a>
                     </div>
                 </div>
                 <div class="col-lg text-center pm-medspor">
                     <div>
-                    <img class="medspor-logo" src="assets/medpart/eventcampus_1.jpg" alt="">
+                    <a href="https://www.instagram.com/event.campus/" target="_blank"><img class="medspor-logo" src="assets/medpart/eventcampus_1.jpg" alt="event.campus"></a>
                     </div>
                 </div>
                 <div class="col-lg text-center pm-medspor">
                     <div>
-                    <img class="medspor-logo" src="assets/medpart/ukmband.jpg" alt="">
+                    <a href="https://www.instagram.com/ukmbandbinus/" target="_blank"><img class="medspor-logo" src="assets/medpart/ukmband.jpg" alt="UKM Band Bina Nusantara"></a>
                     </div>
                 </div>
                 <div class="col-lg text-center pm-medspor">
                     <div>
-                    <img class="medspor-logo" src="assets/medpart/readyradio.jpg" alt="">
+                    <a href="https://www.instagram.com/readyradio/" target="_blank"><img class="medspor-logo" src="assets/medpart/readyradio.jpg" alt="Ready Radio"></a>
                     </div>
                 </div>
             </div> 
@@ -126,22 +121,27 @@
                 <div class="row">
                 <div class="col-lg text-center pm-medspor">
                     <div>
-                    <img class="medspor-logo" src="assets/medpart/eventjournal.jpg" alt="">
+                    <a href="https://www.instagram.com/event.journal/" target="_blank"><img class="medspor-logo" src="assets/medpart/eventjournal.jpg" alt="eventjournal"></a>
                     </div>
                 </div>
                 <div class="col-lg text-center pm-medspor">
                     <div>
-                    <img class="medspor-logo" src="assets/medpart/madingevent.png" alt="">
+                    <a href="https://www.instagram.com/madingevent.id/" target="_blank"><img class="medspor-logo" src="assets/medpart/madingevent.png" alt="madingevent"></a>
                     </div>
                 </div>
                 <div class="col-lg text-center pm-medspor">
                     <div>
-                    <img class="medspor-logo" src="assets/medpart/bdgcreativemedia.jpg" alt="">
+                    <a href="https://www.instagram.com/bdgcreativemedia/" target="_blank"><img class="medspor-logo" src="assets/medpart/bdgcreativemedia.jpg" alt="bdgcreativemedia"></a>
                     </div>
                 </div>
                 <div class="col-lg text-center pm-medspor">
                     <div>
-                    <img class="medspor-logo" src="assets/medpart/eventcampus.jpg" alt="">
+                    <a href="https://www.instagram.com/eventcampus/" target="_blank"><img class="medspor-logo" src="assets/medpart/eventcampus.jpg" alt="eventcampus"></a>
+                    </div>
+                </div>
+                <div class="col-lg text-center pm-medspor">
+                    <div>
+                    <a href="https://www.instagram.com/ikomchannel/" target="_blank"><img class="medspor-logo" src="assets/medpart/ikomchannel.jpg" alt="ikomchannel"></a>
                     </div>
                 </div>
             </div> 
