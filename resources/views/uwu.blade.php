3a87b1339fd22143e7a2bf5b433be320438ae779
Adhi MF <66113867+adhiiimf@users.noreply.github.com>
2021-07-18 17:39:01 +0700

    Add Comingsoon gif21

---
+++ resources/views/uwu.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Grab It Fast 2021</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300&display=swap" rel="stylesheet">
+</head>
+<style>
+body {
+	background-color: rgb(231, 231, 231);
+	color: rgb(46, 46, 46);
+}
+.centered {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  /* bring your own prefixes */
+  transform: translate(-50%, -50%);
+}
+.amaze{
+   font-family: 'Fira Code', monospace;
+   background-color: grey;
+}
+</style>
+<body id="amaze">
+    <div class="centered text-center">
+       <span style="font-size: 1.5rem"><b>GIF 2021</b><br>COMING SOON</span>
+    </div>
+</body>
+</html>
